9c69cd2550681c0acb2a5ce1389d2da296bee46e
Kauan <k4uan.arauj0@gmail.com>
2025-10-24 20:10:59 -0300

    Primiro Commit

---
+++ src/main/java/com/delivery_api/Delivery/API/DeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Delivery.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApiApplication.class, args);
+	}
+
+}
